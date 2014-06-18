7199c39c98b3ab9e06d3fcc4b1f8561190551976
Ng, Tzy Luen <tzyluen.ng@gmail.com>
2014-06-18 23:36:56 +0800

    added example4.2

--- chap4/example4.2.c
+++ chap4/example4.2.c
@@ -6,12 +6,49 @@
 /* rudimentary calculator */
 int main(int argc, char *argv[])
 {
-    double sum;
+    double sum, myatof(char []);
     char line[MAXLINE];
-    int getline(char line[], int max);
+    int mygetline(char line[], int max);
 
     sum = 0;
-    while (getline(line, MAXLINE) > 0)
-        printf("\t%g\n", sum += atof(line));
+    while (mygetline(line, MAXLINE) > 0)
+        printf("\t%g\n", sum += myatof(line));
     return 0;
+}
+
+
+double myatof(char s[])
+{
+    double val, power;
+    int i, sign;
+
+    for (i = 0; isspace(s[i]); i++)  /* skip white space */
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        ++i;
+    for (val = 0.0; isdigit(s[i]); ++i)
+        val = 10.0 * val + (s[i] - '0');
+    if (s[i] == '.')
+        ++i;
+    for (power = 1.0; isdigit(s[i]); ++i) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    return sign * val / power;
+}
+
+
+/* mygetline: get line into s, return length */
+int mygetline(char s[], int lim)
+{
+    int c, i;
+    i = 0;
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    if (c == '\n')
+        s[i++] = c;
+    s[i] = '\0';
+
+    return i;
 }
